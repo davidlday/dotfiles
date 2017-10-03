aa4f0d35a3b67480fe7f38c7a7bcd6000cbebdbe
David L. Day <dday376@gmail.com>
2017-10-03 18:04:35 -0400

    Added python installer.

---
+++ install/python.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source ../shell/.function
+
+if is-macos && is-executable brew; then
+  # Use Homebrew on Mac
+  brew install python python3 pyenv pyenv-virtualenv pyenv-virtualenvwrapper
+elif is-executable apt && can-sudo; then
+  # Use native packages on debian systems (for now)
+  sudo apt install -y python python3 python-pip virtualenv virtualenvwrapper
+fi
