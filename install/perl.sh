5e8b018145d5b489c8d35e0d38628a3f81859489
David L. Day <dday376@gmail.com>
2017-10-03 22:39:32 -0400

    Added perl installer and exports using perlbrew.

---
+++ install/perl.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+source ../shell/.function
+
+# Works on MacOS and Linux
+if is-executable curl; then
+  \curl -L https://install.perlbrew.pl | bash
+else
+  \wget -O - https://install.perlbrew.pl | bash
+fi
