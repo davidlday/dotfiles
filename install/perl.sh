4d4810f38cc56a04ad725562de2e79d40bd80bf5
David L. Day <dday376@gmail.com>
2017-10-04 07:21:32 -0400

    Added plenv to install/perl.sh and shell/.expoerts_perl. Will split plenv and perlbrew into two separate installers later.

--- install/perl.sh
+++ install/perl.sh
@@ -3,7 +3,26 @@ source ../shell/.function
 
 # TODO: Consider switching to plenv: https://github.com/tokuhirom/plenv
 
-# Works on MacOS and Linux
+# plenv
+if is-macos; then
+  if can-brew; then
+    brew update
+    brew install plenv
+    brew install perl-build
+  else
+    echo "Skipped: plenv"
+  fi
+elif
+  if is-executable git; then
+    git clone https://github.com/tokuhirom/plenv.git ~/.plenv
+    git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
+    plenv ~/.plenv/bin/rehash
+  else
+    echo "Skipped: plenv"
+  fi
+fi
+
+# Perlbrew.pl
 if is-executable curl; then
   \curl -L https://install.perlbrew.pl | bash
 else
