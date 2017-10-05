b6c9969d0f572f1f98abe34776e9a085e59e7d31
David L. Day <dday376@gmail.com>
2017-10-04 22:11:06 -0400

    Removed perlbrew and moved to plenv.

--- install/perl.sh
+++ install/perl.sh
@@ -1,30 +1,12 @@
 #!/usr/bin/env bash
 source ../shell/.function
 
-# TODO: Consider switching to plenv: https://github.com/tokuhirom/plenv
-
 # plenv
-if is-macos; then
-  if can-brew; then
-    brew update
-    brew install plenv
-    brew install perl-build
-  else
-    echo "Skipped: plenv"
-  fi
-elif
-  if is-executable git; then
-    git clone https://github.com/tokuhirom/plenv.git ~/.plenv
-    git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
-    plenv ~/.plenv/bin/rehash
-  else
-    echo "Skipped: plenv"
-  fi
-fi
-
-# Perlbrew.pl
-if is-executable curl; then
-  \curl -L https://install.perlbrew.pl | bash
+if can-brew; then
+  brew update
+  brew upgrade
+  brew install plenv
+  brew install perl-build
 else
-  \wget -O - https://install.perlbrew.pl | bash
+  echo "Skipped: plenv. Install brew first (brew.sh)."
 fi
