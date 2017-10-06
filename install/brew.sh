e2b6d4b93c6e7b4c579971f68d89c191c42adf7e
David L. Day <dday376@gmail.com>
2017-10-05 23:49:43 -0400

    Finalized brew installer.

--- install/brew.sh
+++ install/brew.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/env bash
-source ../shell/.function
 
 if ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
   echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
 fi
 
-if is-macos; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+if ! is-executable brew; then
+  if is-macos; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    if is-executable apt && can-sudo; then
+      sudo apt install build-essential
+    elif is-executable yum && can-sudo; then
+      sudo yum groupinstall 'Development Tools'
+    fi
+  fi
 fi
 
-source ../shell/.exports_brew
-
-if is-executable brew; then
-  brew update
-  brew upgrade
+if [ -d /home/linuxbrew/.linuxbrew ]; then
+  prepend-path "/home/linuxbrew/.linuxbrew/bin"
+  export PATH
+elif [ -d ~/.linuxbrew ]; then
+  prepend-path "${HOME}/.linuxbrew"
+  export PATH
 fi
+
+dedupe-path
+
+brew update
+brew upgrade
+
+# Install good stuff
+brew install \
+  vim \
+  curl \
+  vim \
+  maven
