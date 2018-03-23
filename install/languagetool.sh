beb088ed0dbabe0cbcc426189b638d0a06a931bd
David L. Day <dday376@gmail.com>
2018-03-22 23:00:35 -0400

    Languagetool installs via brew on all platforms.

--- install/languagetool.sh
+++ install/languagetool.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
+source ../shell/.function
 
 LT_VER=4.0
 
-if can-brew; then
-  brew install languagetool
+if ! can-brew; then
+  source ./brew.sh
 else
-  sudo rm -rf /opt/languagetool
-  sudo mkdir -p /opt/languagetool
-  curl https://languagetool.org/download/LanguageTool-${LT_VER}.zip -o /tmp/languagetool.zip
-  cd /tmp
-  extract ./languagetool.zip
-  sudo cp -r LanguageTool-${LT_VER}/* /opt/languagetool/
-  rm -rf /tmp/LanguageTool-${LT_VER}
-  rm /tmp/languagetool.zip
-  cd -
+  brew install languagetool
 fi
