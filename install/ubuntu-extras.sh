366e0c21cfe2cc451f1da36f67d1f78a75cdf0d3
David L. Day <dday376@gmail.com>
2018-04-26 07:20:48 -0400

    Added arch-theme install.

--- install/ubuntu-extras.sh
+++ install/ubuntu-extras.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# Themes
-sudo add-apt-repository ppa:noobslab/themes
-
 # Etcher
 echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/sources.list.d/etcher.list
 sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D401AB61
 
 sudo apt-get update
-sudo apt-get install etcher-electron
+sudo apt-get install etcher-electron arc-theme
