8dba58cfe23f6a0d5c6d188b507d1f78819c13e7
David L. Day <dday376@gmail.com>
2018-04-30 18:48:35 -0400

    Added minimal kindlegen downloader.

---
+++ install/kindlegen.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+KGEN_LINK="http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz"
+
+wget --progress=bar -q ${KGEN_LINK} -O /tmp/kindlegen_linux.tar.gz -q --show-      progress
