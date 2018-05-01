b4d89453550b4b25b0f090f73a83d1553704d812
David L. Day <dday376@gmail.com>
2018-05-01 06:59:19 -0400

    Started version compare for atom install.

--- install/atom.sh
+++ install/atom.sh
@@ -3,14 +3,21 @@ source ../shell/.function
 
 # Get installed version
 if is-executable atom; then
-    export INSTALLED_VERSION=`atom --version | grep Atom | cut -d":" -f2`
+    # https://gist.github.com/maxrimue/ca69ee78081645e1ef62
+    INSTALLED_VERSION=$(atom --version | grep Atom | cut -d":" -f2)
+    INSTALLED_VERSION=${INSTALLED_VERSION//./}
+    INSTALLED_MAJOR=$(echo ${INSTALLED_VERSION} | awk '{print $1}')
+    INSTALLED_MINOR=$(echo ${INSTALLED_VERSION} | awk '{print $2}')
+    INSTALLED_PATCH=$(echo ${INSTALLED_VERSION} | awk '{print $3}')
 else
-    INSTALLED_VERSION="0.0.0"
+    INSTALLED_MAJOR="0"
+    INSTALLED_MINOR="0"
+    INSTALLED_PATCH="0"
 fi
 
 wget -q https://github.com/atom/atom/releases/latest -O /tmp/latest
-export GITHUB_URL=`cat /tmp/latest | grep -o -E 'href="([^"#]+)atom-amd64.deb"' | cut -d'"' -f2 | sort | uniq`
-export GITHUB_VERSION=`echo ${GITHUB_URL} | cut -d"/" -f6`
+GITHUB_URL=`cat /tmp/latest | grep -o -E 'href="([^"#]+)atom-amd64.deb"' | cut -d'"' -f2 | sort | uniq`
+GITHUB_VERSION=`echo ${GITHUB_URL} | cut -d"/" -f6`
 # TODO: Test INSTALLED_VERSION against GITHUB_VERSION
 wget --progress=bar -q https://github.com${GITHUB_URL} -O /tmp/atom-amd64.deb -q --show-      progress
 gnome-software --local-filename=/tmp/atom-amd64.deb
