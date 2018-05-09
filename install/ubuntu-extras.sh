88a9529b8a90020bcd1ddac295c9bee80da11a87
David L. Day <dday376@gmail.com>
2018-05-08 21:00:56 -0400

    Added gnome-settings import.

--- install/ubuntu-extras.sh
+++ install/ubuntu-extras.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source ../shell/.function
 
 # Etcher
 echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/sources.list.d/etcher.list
@@ -6,3 +7,7 @@ sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D401AB61
 
 sudo apt-get update
 sudo apt-get install etcher-electron arc-theme
+
+# Restore GNOME Terminal settings
+# https://askubuntu.com/questions/967517/backup-gnome-terminal
+dconf load /org/gnome/terminal < ../gnome-terminal/gnome_terminal_profile.txt
