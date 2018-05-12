cdf1b2bb4abc92ef3c25421eb4a3d03bfdfa432b
David L. Day <dday376@gmail.com>
2018-05-12 16:11:04 -0400

    Completed kindlegen install for Linux.

--- install/kindlegen.sh
+++ install/kindlegen.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+source ../shell/.function
 
-KGEN_LINK="http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz"
+if ! is-macos; then
+    KGEN_LINK="http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz"
+    TMPDIR=$(mktemp -d)
 
-wget --progress=bar -q ${KGEN_LINK} -O /tmp/kindlegen_linux.tar.gz -q --show-      progress
+    wget --progress=bar -q ${KGEN_LINK} -O ${TMPDIR}/kindlegen_linux.tar.gz -q --show-      progress
+    sudo mkdir -p /opt/KindleGen
+    sudo tar --extract --verbose --gzip --file=${TMPDIR}/kindlegen_linux.tar.gz --directory=/opt/KindleGen 
+    sudo chown -R root:root /opt/KindleGen
+    sudo ln -svf /opt/KindleGen/kindlegen /usr/local/bin/kindlegen
+#    rm -rf ${TMPDIR}
+else
+    # TODO: Add Mac install
+    echo "Mac installer not supported yet."
+fi
