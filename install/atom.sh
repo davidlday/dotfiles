9066f7c831123d992416e46be76fc57fe3b8bb53
David L. Day <dday376@gmail.com>
2018-05-23 23:22:11 -0400

    Simplified version parsing.

--- install/atom.sh
+++ install/atom.sh
@@ -3,17 +3,16 @@ source ../shell/.function
 
 # Get installed version
 if is-executable atom; then
-    # https://gist.github.com/maxrimue/ca69ee78081645e1ef62
-    INSTALLED_VERSION=$(atom --version | grep Atom | cut -d":" -f2)
-    INSTALLED_VERSION=${INSTALLED_VERSION//./}
-    INSTALLED_MAJOR=$(echo ${INSTALLED_VERSION} | awk '{print $1}')
-    INSTALLED_MINOR=$(echo ${INSTALLED_VERSION} | awk '{print $2}')
-    INSTALLED_PATCH=$(echo ${INSTALLED_VERSION} | awk '{print $3}')
+  # https://gist.github.com/maxrimue/ca69ee78081645e1ef62
+  INSTALLED_VERSION=$(atom --version | grep Atom | cut -d":" -f2)
+  INSTALLED_VERSION=${INSTALLED_VERSION//./}
 else
-    INSTALLED_MAJOR="0"
-    INSTALLED_MINOR="0"
-    INSTALLED_PATCH="0"
+  INSTALLED_VERSION="0.0.0"
 fi
+
+INSTALLED_MAJOR=$(echo ${INSTALLED_VERSION} | awk '{print $1}')
+INSTALLED_MINOR=$(echo ${INSTALLED_VERSION} | awk '{print $2}')
+INSTALLED_PATCH=$(echo ${INSTALLED_VERSION} | awk '{print $3}')
 
 wget -q https://github.com/atom/atom/releases/latest -O /tmp/latest
 GITHUB_URL=`cat /tmp/latest | grep -o -E 'href="([^"#]+)atom-amd64.deb"' | cut -d'"' -f2 | sort | uniq`
