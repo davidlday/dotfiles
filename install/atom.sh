c15191dfa045fdac29c5cc79830fd8971feb4fca
David L. Day <dday376@gmail.com>
2018-05-28 17:52:33 -0400

    Updated atom installer to do version comparisons.

--- install/atom.sh
+++ install/atom.sh
@@ -5,18 +5,23 @@ source ../shell/.function
 if is-executable atom; then
   # https://gist.github.com/maxrimue/ca69ee78081645e1ef62
   INSTALLED_VERSION=$(atom --version | grep Atom | cut -d":" -f2)
-  INSTALLED_VERSION=${INSTALLED_VERSION//./}
 else
   INSTALLED_VERSION="0.0.0"
 fi
 
-INSTALLED_MAJOR=$(echo ${INSTALLED_VERSION} | awk '{print $1}')
-INSTALLED_MINOR=$(echo ${INSTALLED_VERSION} | awk '{print $2}')
-INSTALLED_PATCH=$(echo ${INSTALLED_VERSION} | awk '{print $3}')
-
 wget -q https://github.com/atom/atom/releases/latest -O /tmp/latest
 GITHUB_URL=`cat /tmp/latest | grep -o -E 'href="([^"#]+)atom-amd64.deb"' | cut -d'"' -f2 | sort | uniq`
 GITHUB_VERSION=`echo ${GITHUB_URL} | cut -d"/" -f6`
-# TODO: Test INSTALLED_VERSION against GITHUB_VERSION
-wget --progress=bar -q https://github.com${GITHUB_URL} -O /tmp/atom-amd64.deb -q --show-      progress
-gnome-software --local-filename=/tmp/atom-amd64.deb
+GITHUB_VERSION=${GITHUB_VERSION//v/}
+
+echo "Installed version: ${INSTALLED_VERSION}"
+echo "GitHub version: ${GITHUB_VERSION}"
+
+ret=$(version_compare ${INSTALLED_VERSION} ${GITHUB_VERSION})
+if [ "${ret}" == "-1" ]; then
+  echo "Installing latest version."
+  wget --progress=bar -q https://github.com${GITHUB_URL} -O /tmp/atom-amd64.deb -q --show-      progress
+  gnome-software --local-filename=/tmp/atom-amd64.deb
+else
+  echo "Atom is up-to-date. Nothing to do."
+fi
