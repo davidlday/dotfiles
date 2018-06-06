50a65dc20a901685399fa17224b64a400eb53327
David L. Day <dday376@gmail.com>
2018-06-05 20:53:15 -0400

    Python install via brew on Linux and Mac.

--- install/python.sh
+++ install/python.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 source ../shell/.function
 
-if is-macos; then
-  if ! can-brew; then
-    source ./brew.sh
-  fi
-  # Use Homebrew on Mac
-  brew install python python@2
-elif is-executable apt && can-sudo; then
-  # Use native packages on debian systems (for now)
-  sudo apt install -y python python3 python-pip virtualenv virtualenvwrapper
+# Use brew on Linux and Mac
+if ! can-brew; then
+  source ./brew.sh
 fi
+# Use Homebrew on Mac
+brew install python python@2
