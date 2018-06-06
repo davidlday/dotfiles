893a79e0ec15991a8fac07dccb31c8ef2384491e
David L. Day <dday376@gmail.com>
2018-06-05 20:22:21 -0400

    Made istats.sh install only work on Mac.

--- install/istats.sh
+++ install/istats.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
+source ../shell/.function
 
-# install
-gem install iStats
+if is-macos; then
+  # install
+  gem install iStats
 
-# sensors for cthulhu
-declare -a sensors=(TA0p TA0P TG0D TL0P TPCD TCGc TCXc TC0c TC1c TC2c TC3c TM0P TM1P TM2P TM3P TH0P)
+  # sensors for cthulhu
+  declare -a sensors=(TA0p TA0P TG0D TL0P TPCD TCGc TCXc TC0c TC1c TC2c TC3c TM0P TM1P TM2P TM3P TH0P)
 
-for i in "${sensors[@]}"
-do
-  echo "Enabling '$i'"
-  istats enable $i
-done
-
+  for i in "${sensors[@]}"
+  do
+    echo "Enabling '$i'"
+    istats enable "$i"
+  done
+else
+  echo "Uh-oh, you're not on a Mac! iStats is only for Macs."
+fi
