3ab0564600bcba4d0801d4ae223b41bdcb2b1c57
David L. Day <dday376@gmail.com>
2018-06-05 20:51:26 -0400

    Cleaned up kindlegen.sh per shellcheck.

--- install/kindlegen.sh
+++ install/kindlegen.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 source ../shell/.function
 
-if ! is-macos; then
+# TODO: Figure out how to do upgrades.
+if ! is-macos && ! is-executable kindlegen; then
     KGEN_LINK="http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz"
-    TMPDIR=$(mktemp -d)
+    TMPDIR=$(mktemp -d /tmp/kindlegen-XXXXXXXX)
 
-    wget --progress=bar -q ${KGEN_LINK} -O ${TMPDIR}/kindlegen_linux.tar.gz -q --show-      progress
+    wget --progress=bar -q "$KGEN_LINK" -O "$TMPDIR/kindlegen_linux.tar.gz" -q --show-      progress
     sudo mkdir -p /opt/KindleGen
-    sudo tar --extract --verbose --gzip --file=${TMPDIR}/kindlegen_linux.tar.gz --directory=/opt/KindleGen 
+    sudo tar --extract --verbose --gzip --file="$TMPDIR/kindlegen_linux.tar.gz" --directory=/opt/KindleGen
     sudo chown -R root:root /opt/KindleGen
     sudo ln -svf /opt/KindleGen/kindlegen /usr/local/bin/kindlegen
 #    rm -rf ${TMPDIR}
