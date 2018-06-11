18c665bb3f4105391435ce68d1e7c5173a2de2bd
David L. Day <dday376@gmail.com>
2018-06-11 10:55:59 -0400

    Changed .extras to .loca/dotfiles for machine-specific files and cleaned up based on shellcheck.

--- setup.sh
+++ setup.sh
@@ -2,17 +2,16 @@
 
 # Get current dir (so run this script from anywhere)
 
-export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
+export DOTFILES_DIR DOTFILES_CACHE DOTFILES_LOCAL_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
-DOTFILES_EXTRA_DIR="$HOME/.extra"
+DOTFILES_LOCAL_DIR="$HOME/.local/dotfiles"
 
 # Common functions
-# shellcheck source=shell/.function
+# shellcheck source=/dev/null
 source "$DOTFILES_DIR/shell/.function"
 
 # Update dotfiles itself first
-
 if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
 
 # Ensure directories exists
@@ -43,7 +42,7 @@ ln -sfv "$DOTFILES_DIR/cookiecutter/.cookiecutterrc" "$HOME/.cookiecutterrc"
 if is-executable bats; then bats test/*.bats; else echo "Skipped: tests (missing: bats)"; fi
 
 # Install extra stuff
-if [ -d "$DOTFILES_EXTRA_DIR" ] && [ -f "$DOTFILES_EXTRA_DIR/install.sh" ]; then
+if [ -d "$DOTFILES_LOCAL_DIR" ] && [ -f "$DOTFILES_LOCAL_DIR/install.sh" ]; then
   # shellcheck source=/dev/null
-  source "$DOTFILES_EXTRA_DIR/install.sh"
+  source "$DOTFILES_LOCAL_DIR/install.sh"
 fi
