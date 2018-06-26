065733acb920f397309a0e3b2f4381aaec51d9c3
David L. Day <dday@localhost.localdomain>
2018-06-26 00:11:06 -0400

    Brew installer updated for CentOS.

--- install/brew.sh
+++ install/brew.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 source ../shell/.function
 
-# Install Homebrew or linuxbrew
-if ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
-  echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
-  exit 1
-fi
-
 if ! can-brew; then
   if is-macos; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Installing HomeBrew (https://brew.sh)"
+    # See: https://brew.sh/
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   else
-    if is-executable apt && can-sudo; then
-      sudo apt-get --yes install build-essential curl file git python-setuptools ruby
-    elif is-executable yum && can-sudo; then
-      sudo yum groupinstall 'Development Tools' && sudo yum install curl file git python-setuptools ruby
-    fi
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-    if [ -d /home/linuxbrew/.linuxbrew ]; then
-      prepend-path "/home/linuxbrew/.linuxbrew/bin"
-      export PATH
-    elif [ -d "$HOME/.linuxbrew" ]; then
-      prepend-path "${HOME}/.linuxbrew/bin"
-      export PATH
+    if can-sudo; then
+      echo "Installing LinuxBrew (https://linuxbrew.sh)"
+      if is-executable apt; then
+        sudo apt-get --yes install build-essential curl file git python-setuptools ruby
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+      elif is-executable dnf; then
+        sudo dnf groupinstall 'Development Tools' && sudo dnf install curl file git
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+      elif is-executable yum; then
+        sudo yum groupinstall 'Development Tools' && sudo yum install curl file git
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+      else
+        echo "Dudo, you're not on a Mac and you can't apt, dnf, or yum. What the hell platform you on, bro?"
+      fi
+    else
+      echo "Need sudo to install LinuxBrew, bro!"
     fi
   fi
 else
   echo "Brew already installed. Running updates."
 fi
+
+source ../shell/.exports_brew
 
 dedupe-path
 
@@ -45,7 +47,7 @@ brew install \
   cookiecutter \
   curl \
   git \
-  bash-git-prompt
+  bash-git-prompt \
   maven \
   pandoc \
   python \
