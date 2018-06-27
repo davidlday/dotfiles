dd185e0f9d21ffde4c3b380e9c5f0717999b5b56
David L. Day <dday376@gmail.com>
2018-06-26 23:32:13 -0400

    Added installer for pyenv.

---
+++ install/pyenv.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Not strictly necessary, but left over from old readlink/greadlink.
+REALPATH=$(command -v realpath)
+
+# Find current script source, if possible
+CURRENT_SCRIPT="${BASH_SOURCE[0]}"
+
+# Resolve DOTFILES_DIR (assuming ~/.dotfiles on distros without realpath and/or $BASH_SOURCE/$0)
+if [[ -n $CURRENT_SCRIPT && -x "$REALPATH" ]]; then
+  SCRIPT_PATH=$($REALPATH "$CURRENT_SCRIPT")
+  DOTFILES_DIR=$(dirname "$(dirname "$SCRIPT_PATH")")
+elif [ -d "$HOME/.dotfiles" ]; then
+  DOTFILES_DIR="$HOME/.dotfiles"
+else
+  echo "Unable to find dotfiles, exiting."
+  return
+fi
+
+# Source functions, assuming they're not already
+# shellcheck source=/dev/null
+source "$DOTFILES_DIR/shell/.function"
+
+if can-sudo; then
+  if can-apt; then
+    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+      libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+      xz-utils tk-dev libffi-dev
+  elif can-dnf; then
+    sudo dnf install zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel
+  elif can-yum; then
+    sudo yum install zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel
+  else
+    echo "Bro, can't apt, dnf, or yum! What platform you on?"
+    exit 1
+  fi
+  if ! can-brew; then
+    # shellcheck source=/dev/null
+    source "$DOTFILES_DIR/install/brew.sh"
+  fi
+  brew install pyenv pyenv-virtualenv pyenv-virtualenvwrapper
+else
+  echo "Can't sudo, bro! Even with brew ya need sudo!"
+  exit 1
+fi
