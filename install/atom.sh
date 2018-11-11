eac47f6bebbda15d91e017c1d395814248135266
David L. Day <dday376@gmail.com>
2018-11-11 13:42:16 -0500

    Atom now has a repo for install / update.

--- install/atom.sh
+++ install/atom.sh
@@ -21,35 +21,33 @@ fi
 # shellcheck source=/dev/null
 source "$DOTFILES_DIR/shell/.function"
 
-if ! is-macos; then
-  # Get installed version
-  if is-executable atom; then
-    # https://gist.github.com/maxrimue/ca69ee78081645e1ef62
-    INSTALLED_VERSION=$(atom --version | grep Atom | cut -d":" -f2 | xargs)
+if ! is-executable atom; then
+  if ! is-macos; then
+    if can-apt; then
+      curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+      sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+      sudo apt-get update
+      sudo apt-get install atom
+    elif is-executable rpm; then
+      sudo rpm --import https://packagecloud.io/AtomEditor/atom/gpgkey
+      sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo'
+      if can-dnf; then
+        sudo dnf check-update
+        sudo dnf install atom
+      elif can-yum; then
+        sudo yum --assumeyes check-update
+        sudo yum --assumeyes install atom
+      else
+        echo "Hmmm - rpm is available, but dnf and yum aren't. What the heck, bro?"
+      fi
+    else
+      echo "Can't use apt or rpm. What the heck, bro?"
+    fi
   else
-    INSTALLED_VERSION="0.0.0"
-  fi
-
-  ATOM_LATEST=$(mktemp /tmp/atom-latest-XXXXXXXX)
-  wget --show-progres --progress=bar -q https://github.com/atom/atom/releases/latest -O "$ATOM_LATEST"
-  GITHUB_URL=$(< "$ATOM_LATEST" grep -o -E 'href="([^"#]+)atom-amd64.deb"' | cut -d'"' -f2 | sort | uniq)
-  GITHUB_VERSION=$(echo "$GITHUB_URL" | cut -d"/" -f6)
-  GITHUB_VERSION="${GITHUB_VERSION//v/}"
-  rm -rf "$ATOM_LATEST"
-
-  echo "Installed version: '$INSTALLED_VERSION'"
-  echo "GitHub version: '$GITHUB_VERSION'"
-
-  if [ "$INSTALLED_VERSION" != "$GITHUB_VERSION" ]; then
-    echo "Installing latest version from https://atom.io/download/deb"
-    wget --show-progres --progress=bar -q "https://atom.io/download/deb" -O /tmp/atom-amd64.deb
-    sudo dpkg -i /tmp/atom-amd64.deb
-    rm /tmp/atom-amd64.deb
-  else
-    echo "Atom is up-to-date. Nothing to do."
+    echo "Man, you're on a Mac! Download and install it yerself, fool!"
   fi
 else
-  echo "Man, you're on Mac! Atom updates automatically."
+  echo "Looks like Atom's already installed. Try running an update, bro!"
 fi
 
 if is-executable apm; then
@@ -75,7 +73,7 @@ if is-executable apm; then
   )
 
   for package in "${packages[@]}"; do
-    if ls "$HOME/.atom/packages" | grep "$package" >/dev/null 2>&1; then
+    if [ -d "$HOME/.atom/packages/$package" ]; then
       echo "$package already installed."
     else
       apm install "$package"
