135c045f5117f3b59f7a07c7c85f91355c239b46
David L. Day <dday376@gmail.com>
2019-03-17 16:28:33 -0400

    simplified sourcing .functions

--- install/docker.sh
+++ install/docker.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
-
-# Not strictly necessary, but left over from old readlink/greadlink.
-REALPATH=$(command -v realpath)
-
-# Find current script source, if possible
-CURRENT_SCRIPT="${BASH_SOURCE[0]}"
-
-# Resolve DOTFILES_DIR (assuming ~/.dotfiles on distros without realpath and/or $BASH_SOURCE/$0)
-if [[ -n "$CURRENT_SCRIPT" && -x "$REALPATH" ]]; then
-  SCRIPT_PATH=$($REALPATH "$CURRENT_SCRIPT")
-  DOTFILES_DIR=$(dirname "$(dirname "$SCRIPT_PATH")")
-elif [ -d "$HOME/.dotfiles" ]; then
-  DOTFILES_DIR="$HOME/.dotfiles"
-else
-  echo "Unable to find dotfiles, exiting."
-  return
-fi
-
-# Source functions, assuming they're not already
-# shellcheck source=/dev/null
-source "$DOTFILES_DIR/shell/.function"
+source ../shell/.function
 
 if ! is-macos; then
   echo "Installing Docker CE & Docker Compose."
--- install/gnome-terminal.sh
+++ install/gnome-terminal.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 # See: https://askubuntu.com/questions/967517/backup-gnome-terminal
-
-# Not strictly necessary, but left over from old readlink/greadlink.
-REALPATH=$(command -v realpath)
-
-# Find current script source, if possible
-CURRENT_SCRIPT="${BASH_SOURCE[0]}"
-
-# Resolve DOTFILES_DIR (assuming ~/.dotfiles on distros without realpath and/or $BASH_SOURCE/$0)
-if [[ -n "$CURRENT_SCRIPT" && -x "$REALPATH" ]]; then
-  SCRIPT_PATH=$($REALPATH "$CURRENT_SCRIPT")
-  DOTFILES_DIR=$(dirname "$(dirname "$SCRIPT_PATH")")
-elif [ -d "$HOME/.dotfiles" ]; then
-  DOTFILES_DIR="$HOME/.dotfiles"
-else
-  echo "Unable to find dotfiles, exiting."
-  return
-fi
+source ../shell/.function
 
 if ! is-macos; then
   DCONF="$(command -v dconf)"
